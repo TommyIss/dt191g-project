8816f31292f815e1d5d55efaed732bcdf719c11a
Tommy Issa <tommyissa@hotmail.com>
2026-07-25 17:23:27 +0200

    Add project files.

---
+++ dt191g-project/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace dt191g_project.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
